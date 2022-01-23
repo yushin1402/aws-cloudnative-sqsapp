03afdbd81e1123311ff9ddc209c99cda30b849f2
yushin1402 <s.masaomi16h@gmail.com>
2022-01-24 00:27:31 +0900

    Dockerfile for producer app

--- sqs-consumer/src/main/java/org/debugroom/mynavi/sample/aws/sqs/config/SqsConfig.java
+++ sqs-consumer/src/main/java/org/debugroom/mynavi/sample/aws/sqs/config/SqsConfig.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.debugroom.mynavi.sample.aws.sqs.app.batch.*;
 
-@ComponentScan("org.debugroom.mynavi.sample.aws.sqs.app.listener")
+@ComponentScan("org.debugroom.mynavi.sample.aws.sqs.app.listener,org.debugroom.mynavi.sample.aws.sqs.app.batch")
 @Configuration
 public class SqsConfig {
 
